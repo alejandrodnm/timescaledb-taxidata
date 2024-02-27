b52dca0049b6bff9b0d89e22649d58a66d1e8c36
Alejandro Do Nascimento Mora <alejandrodnm@gmail.com>
2024-02-27 11:09:12 +0100

    Add csv and tscopy

--- src/load.py
+++ src/load.py
@@ -15,7 +15,7 @@ os.makedirs("data/converted", exist_ok=True)
 
 
 def handle_file(file, columns, cabid):
-    conn = psycopg2.connect(**conn_dict)
+    # conn = psycopg2.connect(**conn_dict)
 
     print(f"Processing {file} at {time.time()}")
     trips = pq.read_table(file)
@@ -27,22 +27,22 @@ def handle_file(file, columns, cabid):
     trips.to_csv(output, index=False)
     print(f"File {file} converted to csv at {time.time()}")
 
-    cur = conn.cursor()
-    f = open(output, "r")
-    copy_sql = """
-     COPY trips_hyper FROM stdin WITH CSV HEADER
-     DELIMITER as ','
-    """
-    cur.copy_expert(sql=copy_sql, file=f)
-    cur.execute(
-        f"INSERT INTO data_loaded (filename, loaded_at) VALUES ('{file}', now())"
-    )
-    conn.commit()
-    f.close()
-
-    os.remove(output)
-    conn.close()
-    print(f"File {file} loaded to db at {time.time()}")
+    # cur = conn.cursor()
+    # f = open(output, "r")
+    # copy_sql = """
+    #  COPY trips_hyper FROM stdin WITH CSV HEADER
+    #  DELIMITER as ','
+    # """
+    # cur.copy_expert(sql=copy_sql, file=f)
+    # cur.execute(
+    #     f"INSERT INTO data_loaded (filename, loaded_at) VALUES ('{file}', now())"
+    # )
+    # conn.commit()
+    # f.close()
+    #
+    # os.remove(output)
+    # conn.close()
+    # print(f"File {file} loaded to db at {time.time()}")
 
 
 def handle_green(file):
@@ -84,25 +84,25 @@ if __name__ == "__main__":
     concurrency = 2
 
     # Get the files that have already been loaded
-    conn = psycopg2.connect(**conn_dict)
-    cur = conn.cursor()
-    cur.execute("select filename from data_loaded")
-    loaded = cur.fetchall()
-    loaded = set([x[0] for x in loaded])
-    conn.close()
+    # conn = psycopg2.connect(**conn_dict)
+    # cur = conn.cursor()
+    # cur.execute("select filename from data_loaded")
+    # loaded = cur.fetchall()
+    # loaded = set([x[0] for x in loaded])
+    # conn.close()
 
     yellow_files = glob("./data/yellow_tripdata_*.parquet")
-    yellow_files = [x for x in yellow_files if x not in loaded]
+    # yellow_files = [x for x in yellow_files if x not in loaded]
 
     green_files = glob("./data/green_tripdata_*.parquet")
-    green_files = [x for x in green_files if x not in loaded]
+    # green_files = [x for x in green_files if x not in loaded]
 
     with Pool(concurrency) as p:
         p.map(handle_yellow, yellow_files)
         p.map(handle_green, green_files)
 
     # Copy the data from the hyper table to the main table
-    if "copy" not in loaded:
-        handle_copy()
+    # if "copy" not in loaded:
+    #     handle_copy()
 
     print("Done!")
